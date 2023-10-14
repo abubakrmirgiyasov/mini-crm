0895dab697cd97ffb35b9f779576747d633ac34b
Abubakr <abubakrmirgiyasov@gmail.com>
2023-10-14 15:40:09 +0700

    Added project views + crud logic

---
+++ src/MiniCrm.UI/Controllers/ProjectController.cs
@@ -0,0 +1,162 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MiniCrm.UI.Models;
+using MiniCrm.UI.Models.DTO_s;
+using MiniCrm.UI.Repositories.Interfaces;
+namespace MiniCrm.UI.Controllers;
+
+public class ProjectController : Controller
+{
+    private readonly ILogger<ProjectController> _logger;
+    private readonly IProjectRepository _project;
+    private readonly IEmployeeRepository _employee;
+    private readonly IManagerRepository _manager;
+
+    public ProjectController(
+        IEmployeeRepository employee,
+        IProjectRepository project,
+        IManagerRepository manager,
+        ILogger<ProjectController> logger)
+    {
+        _employee = employee;
+        _project = project;
+        _logger = logger;
+        _manager = manager;
+    }
+
+    public async Task<IActionResult> Index(string sortOrder)
+    {
+        try
+        {
+            ViewBag.NameSortParam = !string.IsNullOrEmpty(sortOrder) ? "NameSortParam" : "";
+            ViewBag.NamePerformParam = !string.IsNullOrEmpty(sortOrder) ? "NamePerformParam" : "";
+
+            var projects = await _project.GetProjectsAsync();
+
+            projects = sortOrder switch
+            {
+                "NameSortParam" => projects.OrderByDescending(x => x.Name),
+                "NamePerformParam" => projects.OrderByDescending(x => x.PerformingCompany),
+                "StatusSort" => projects.OrderByDescending(x => x.Priority),
+                _ => projects.OrderBy(x => x.Priority),
+            };
+
+            _logger.LogInformation("Getting all projects, founded {Count}", projects.Count());
+
+            return View(projects);
+        }
+        catch (Exception ex)
+        {
+            _logger.LogInformation("Getting all projects. Error {Message}", ex.Message);
+            return BadRequest(ex.Message);
+        }
+    }
+
+    public ActionResult Create()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Create([FromForm] ProjectBindingModel model)
+    {
+        try
+        {
+            await _project.CreateProjectAsync(model);
+            return RedirectToPage("/project/index");
+        }
+        catch (Exception ex)
+        {
+            ViewData["Error"] = ex.Message;
+            return View();
+        }
+    }
+
+    public async Task<IActionResult> Edit(Guid id)
+    {
+        try
+        {
+            var project = await _project.GetProjectByIdAsync(id);
+            return View(project);
+        }
+        catch (Exception ex)
+        {
+            ViewData["Error"] = ex.Message;
+            return BadRequest(ex.Message);
+        }
+    }
+
+    [HttpPut]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Edit(Guid id, [FromForm] ProjectBindingModel model)
+    {
+        try
+        {
+            model.Id = id;
+            await _project.EditProjectAsync(model);
+            return RedirectToRoute("/");
+        }
+        catch (Exception ex)
+        {
+            ViewData["Error"] = ex.Message;
+            return BadRequest(ex.Message);
+        }
+    }
+
+    public async Task<IActionResult> Delete(Guid id)
+    {
+        try
+        {
+            var project = await _project.GetProjectByIdAsync(id);
+            return View(project);
+        }
+        catch (Exception ex)
+        {
+            ViewData["Error"] = ex.Message;
+            return BadRequest(ex.Message);
+        }
+    }
+
+    [HttpDelete]
+    public async Task<IActionResult> Delete(Guid id, IFormCollection? form)
+    {
+        try
+        {
+            await _project.DeleteProjectAsync(id);
+            return RedirectToAction(nameof(Index));
+        }
+        catch (Exception ex)
+        {
+            ViewData["Error"] = ex.Message;
+            return BadRequest(ex.Message);
+        }
+    }
+
+    public async Task<ActionResult> GetEmployees()
+    {
+        try
+        {
+            var employees = await _employee.GetSampleEmployeesAsync();
+            return Json(employees);
+        }
+        catch (Exception ex)
+        {
+            ViewData["Error"] = ex.Message;
+            return BadRequest(ex.Message);
+        }
+    }
+
+    public async Task<ActionResult> GetManagers()
+    {
+        try
+        {
+            var managers = await _manager.GetSampleManagersAsync();
+            return Json(managers);
+        }
+        catch (Exception ex)
+        {
+            ViewData["Error"] = ex.Message;
+            return BadRequest(ex.Message);
+        }
+    }
+}
---
+++ src/MiniCrm.UI/wwwroot/js/project.js
@@ -0,0 +1,108 @@
+﻿$(document).ready(function () {
+    const employeesSelectElement = $("#projectEmployees");
+    const errorBlock = $("#errorBlock");
+    const errorText = $(".error-text");
+
+    if (employeesSelectElement.length !== 0) {
+        const managersSelectElement = $("#projectManagers");
+        const projectRow = $("#projectRow");
+        const projectLoader = $("#projectLoader");
+
+        projectLoader.show();
+        projectRow.hide();
+
+        $.ajax({
+            type: "GET",
+            url: "/project/getEmployees",
+            dataType: "json",
+            success: function (response) {
+                $.each(response, function (index, item) {
+                    employeesSelectElement.append(new Option(item.label, item.value));
+                    console.log(item);
+                });
+
+                $.each(response, function (index, item) {
+                    managersSelectElement.append(new Option(item.label, item.value));
+                });
+
+                projectLoader.hide();
+                projectRow.show();
+            },
+            error: function (xhr, status, error) {
+                projectLoader.hide();
+                projectRow.show();
+
+                errorBlock.show();
+                errorText.text(error);
+
+                console.log(`Error: ${error}, Status: ${status}`);
+            }
+        });
+    }
+
+    const formAdd = $(".project-action");
+
+    formAdd.submit(function (e) {
+        e.preventDefault();
+
+        if (formAdd.valid()) {
+            $.ajax({
+                type: "POST",
+                url: "/project/create",
+                data: formAdd.serialize(),
+                success: function (response) {
+                    window.location.replace("/project");
+                },
+                error: function (xhr, status, error) {
+                    errorBlock.show();
+                    errorText.text(error);
+
+                    console.log(`Error: ${error}, Status: ${status}`);
+                }
+            });
+        }
+    });
+
+    const formEdit = $(".project-edit");
+
+    formEdit.submit(function (e) {
+        e.preventDefault();
+
+        const url = window.location.pathname;
+        const id = url.substring(url.lastIndexOf("/") + 1);
+
+        if (formEdit.valid()) {
+            $.ajax({
+                type: "PUT",
+                url: "/project/edit/" + id,
+                data: formEdit.serialize(),
+                success: function (e) {
+                    window.location.replace("/project");
+                },
+                error: function (xhr, status, error) {
+                    errorBlock.show();
+                    errorText.text(error);
+                }
+            });
+        }
+    });
+
+    const deleteProjectButton = $("#deleteProject");
+
+    deleteProjectButton.click(function (e) {
+        const url = window.location.pathname;
+        const id = url.substring(url.lastIndexOf("/") + 1);
+
+        $.ajax({
+            type: "DELETE",
+            url: "/project/delete/" + id,
+            success: function (response) {
+                window.location.replace("/");
+            },
+            error: function (xhr, status, error) {
+                errorBlock.show();
+                errorText.text(error);
+            }
+        });
+    });
+});
