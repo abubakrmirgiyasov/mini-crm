c91081edda1386ed2acf7e04a1d927c6ae895e9f
Abubakr <abubakrmirgiyasov@gmail.com>
2023-10-14 13:21:02 +0700

    Init

---
+++ tests/MiniCrm.RectTS.Test/Controllers/HomeController.cs
@@ -0,0 +1,10 @@
+namespace MiniCrm.RectTS.Test.Controllers;
+
+public class HomeController
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ tests/MiniCrm.UI.Test/Controllers/HomeController.cs
@@ -0,0 +1,10 @@
+namespace MiniCrm.UI.Test.Controllers;
+
+public class HomeController
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
